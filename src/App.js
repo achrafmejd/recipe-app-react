c847692ccc1f09a237c76526475b5305bd7cbda1
achrafMD <achraf.mejd@ensam-casa.ma>
2021-12-11 18:55:09 +0100

    Router added, just home & allRecipes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Api from "./components/Api";
 import RecipeList from "./pages/RecipeList";
 import Home from "./pages/Home";
 import Recipe from "./pages/Recipe";
+import { BrowserRouter as Router, Route , Routes} from "react-router-dom";
+
 
 
 function App(){
     return(
-        <>
-            <Recipe />
-        </>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home />}>
+                </Route>
+                <Route path="/all-recipes" element={<RecipeList /> }>
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 
